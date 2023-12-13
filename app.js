a4adf3b8117ad607bf2516d2171609b749e9a257
T-Man-CanonEvent <tejassrirama1@gmail.com>
2023-12-12 17:42:21 -0800

    Updated Files

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ async function searchBooks() {
     const query = document.querySelector('input').value;
     const resultsDiv = document.getElementById('results');
     resultsDiv.innerHTML = '<p>Loading....</p>';
-
     try {
         const response = await fetch(`/api/search-books/${query}`);
         const htmlResult = await response.text();
