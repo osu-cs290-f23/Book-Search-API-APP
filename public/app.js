7c196176f51b7d28c7edfb418acc760fccbed17f
Murali Inti <intim@oregonstate.edu>
2023-12-13 10:02:10 -0800

    Search Function Implemented.

--- public/app.js
+++ public/app.js
@@ -1,13 +1,44 @@
-async function searchBooks() {
-    const query = document.querySelector('input').value;
-    const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = '<p>Loading....</p>';
-    try {
-        const response = await fetch(`/api/search-books/${query}`);
-        const htmlResult = await response.text();
-        resultsDiv.innerHTML = htmlResult;
-    } catch (error) {
-        console.error(error);
-        resultsDiv.innerHTML = '<p>Error fetching data</p>';
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("search-input");
+    const searchButton = document.getElementById("search-button");
+    const searchResults = document.querySelector(".search-results");
+  
+    searchButton.addEventListener("click", function () {
+      const searchTerm = searchInput.value.trim();
+  
+      if (searchTerm !== "") {
+        const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(searchTerm)}`;
+  
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => {
+            displayResults(data.items);
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
+      }
+    });
+  
+    function displayResults(results) {
+      searchResults.innerHTML = ""; // Clear previous results
+  
+      if (results && results.length > 0) {
+        const resultList = document.createElement("ul");
+  
+        results.forEach((result) => {
+          const listItem = document.createElement("li");
+          const title = result.volumeInfo.title;
+          const authors = result.volumeInfo.authors ? result.volumeInfo.authors.join(", ") : "Unknown Author";
+  
+          listItem.innerHTML = `<strong>${title}</strong> by ${authors}`;
+          resultList.appendChild(listItem);
+        });
+  
+        searchResults.appendChild(resultList);
+      } else {
+        searchResults.innerHTML = "<p>No results found.</p>";
+      }
     }
-}
+  });
+  
--- server.js
+++ server.js
@@ -1,49 +1,18 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const MongoClient = require('mongodb').MongoClient;
+const PORT = process.env.PORT || 3000;
 
-const url = 'mongodb://localhost:27017'; // Update with your MongoDB connection string
-const dbName = 'bookstore'; // Update with your database name
-const collectionName = 'books'; // Update with your collection name
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware
+app.use(express.static('public'));
 app.use(express.json());
 
-let db; // MongoDB database connection
-
-// Connect to MongoDB
-MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-    if (err) {
-        console.error('Error connecting to MongoDB:', err);
-        return;
-    }
-    console.log('Connected to MongoDB');
-    db = client.db(dbName);
-});
-
-// Define a route to handle book searches
-app.get('/search', async (req, res) => {
-    try {
-        const searchTerm = req.query.query.toLowerCase();
-
-        // Use MongoDB to find books based on the search term
-        const results = await db.collection(collectionName)
-            .find({ title: { $regex: searchTerm, $options: 'i' } })
-            .toArray();
-
-        res.json(results);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
+// Default route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+    res.sendFile(__dirname + '/public/home.html');
 });
 
-const PORT = process.env.PORT || 3000;
+// Start server
 app.listen(PORT, () => {
     console.log("Server is running on Port 3000");
 });
