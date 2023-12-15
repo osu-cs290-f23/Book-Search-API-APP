9634947936597b8065dd94323e3e3e6599ca1307
kaveenaya <kaveeom@gmail.com>
2023-12-15 00:07:15 -0800

    fixed books container alignment issue

--- public/app.js
+++ public/app.js
@@ -23,63 +23,61 @@ function addBook(title, image, author) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const searchInput = document.getElementById("search-input");
-    const searchButton = document.getElementById("search-button");
-    const searchResults = document.querySelector(".search-results");
-  
-    searchButton.addEventListener("click", function () {
+  const searchInput = document.getElementById("search-input");
+  const searchButton = document.getElementById("search-button");
+  const searchResults = document.querySelector(".search-results");
+  const booksContainer = document.getElementById("books-container");
+
+  searchButton.addEventListener("click", function () {
       const searchTerm = searchInput.value.trim();
-          // Create search results heading
 
       if (searchTerm === "") {
-        alert("Please enter a book title.");
-        return;
+          alert("Please enter a book title.");
+          return;
       }
 
       if (searchTerm !== "") {
+          const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(searchTerm)}`;
 
-        const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(searchTerm)}`;
-  
-        fetch(apiUrl)
-          .then((response) => response.json())
-          .then((data) => {
-            displayResults(data.items);
-          })
-          .catch((error) => {
-            console.error("Error fetching data:", error);
-          });
+          fetch(apiUrl)
+              .then((response) => response.json())
+              .then((data) => {
+                  displayResults(data.items);
+              })
+              .catch((error) => {
+                  console.error("Error fetching data:", error);
+              });
       }
-    });
-  
-    function displayResults(results) {
-      searchResults.innerHTML = ""; // Clear previous results
+  });
+
+  function displayResults(results) {
+      searchResults.innerHTML = ""; // Clear previous resultsS
       const searchResultsHeading = document.createElement("h2");
       searchResultsHeading.textContent = "Search Results:";
       searchResults.appendChild(searchResultsHeading);
-     
-      if (results && results.length > 0) {
-         results.forEach((result) => {
-           const bookContainer = document.createElement("div");
-           bookContainer.className = "book-container";
-     
-           const bookImg = result.volumeInfo.imageLinks.thumbnail;
-           const title = result.volumeInfo.title;
-           const authors = result.volumeInfo.authors ? result.volumeInfo.authors.join(", ") : "Unknown Author";
-     
-           bookContainer.innerHTML = `
-             <img src = "${bookImg}" style = "max-width: 100px; max-height: 150px; margin-top: 50px">
-             <br><br>
-             <strong>${title}</strong> by ${authors} 
-             <form onsubmit = "event.preventDefault(); addBook('${title}', '${bookImg}', '${authors}')">
-               <input type = "Submit" class = "plus-button" value = "❤">
-             </form>
-             <br><br>
-           `;
-           searchResults.appendChild(bookContainer);
-         });
-      } else {
-         searchResults.innerHTML = "<p> No results found. </p>";
-      }
-     }
 
-  });
+      if (results && results.length > 0) {
+          results.forEach((result) => {
+              const bookContainer = document.createElement("div");
+              bookContainer.className = "book-container";
+
+              const bookImg = result.volumeInfo.imageLinks.thumbnail;
+              const title = result.volumeInfo.title;
+              const authors = result.volumeInfo.authors ? result.volumeInfo.authors.join(", ") : "Unknown Author";
+
+              bookContainer.innerHTML = `
+                  <img src="${bookImg}" style="max-width: 100px; max-height: 150px; margin-top: 50px">
+                  <br><br>
+                  <strong>${title}</strong> by ${authors} 
+                  <form onsubmit="event.preventDefault(); addBook('${title}', '${bookImg}', '${authors}')">
+                      <input type="Submit" class="plus-button" value="❤">
+                  </form>
+                  <br><br>
+              `;
+              booksContainer.appendChild(bookContainer);
+          });
+      } else {
+          searchResults.innerHTML = "<p>No results found.</p>";
+      }
+  }
+});
